240907a02df6c22ef49da42aabf9540d633ce00d
Tristan Schrader <9901294+schradert@users.noreply.github.com>
2021-08-14 05:38:12 +0000

    Refactored the original Banner component into a container BannerBase (for all sections on mobile to use) and a BannerConnector to get the shape of the head and tail of the elongated vertical chevrons correct (this goes between sections).

---
+++ src/BannerBase.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let color;
+</script>
+
+<div class="banner" style={`background-color: ${color}`}>
+  <slot>
+    Why you no put content here ehhh
+  </slot>
+</div>
+
+<style>
+  .banner {
+    width: clamp(360px, 30vw, 500px);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    gap: 2rem;
+
+    padding: 5%;
+    /* margin: auto; */
+  }
+</style>
