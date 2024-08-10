b966a1de082877a278c137676d7b854063408286
Tristan Schrader <tristan@climaxfoods.com>
2024-08-10 15:51:18 -0700

    chore: pre-commit fix
    allow arbitrary line length in markdown... this should be a default

--- nix/default.nix
+++ nix/default.nix
@@ -66,15 +66,15 @@
     in "${bun} install && ${bun} dev";
     canivete.pre-commit = {
       languages.javascript.enable = true;
-      # Also run biome on .svelte files
-      settings.hooks.biome.types_or = ["svelte"];
       settings.excludes = ["todo"];
-      settings.hooks.lychee.settings.flags = nix.concatStringsSep " " [
-        # TODO migrate to a different data store
-        "--exclude storage.googleapis.com"
+      settings.hooks = {
+        # Also run biome on .svelte files
+        biome.types_or = ["svelte"];
+        # Allow arbitrary line length in markdown (paragraph wrapping preferred)
+        markdownlint.settings.configuration.MD013.line_length = -1;
         # Remap sveltekit assets to correct folder for static link checking
-        "--remap 'src/%25sveltekit.assets%25 static'"
-      ];
+        lychee.settings.flags = "--remap 'src/%25sveltekit.assets%25 static'";
+      };
     };
     canivete.kubenix.clusters.prod.modules.main.kubernetes.resources.deployments.spec = let
       labels.app = "me";
